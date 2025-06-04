b59c7d31f4b794239f249faa91396aefa869c93d
Soumyarup Chaudhury <srch3600@gmail.com>
2025-06-05 00:11:43 +0530

    removed redundant code

--- messenger/controller.js
+++ messenger/controller.js
@@ -18,51 +18,6 @@ exports.postMessage = async (req, res) => {
     try {
         result = await axios.post(url = 'http://127.0.0.1:8000/moderate', data = { 'message': message })
 
-        // const python = spawnSync('python', ['./messenger/automod.py'], {
-        //     stdio: 'pipe',
-        //     encoding: 'utf-8',
-        //     input: message
-        // })
-
-        // output = python.output[1].trim()
-
-        // if (output === 'False') {
-        //     await pool.query("INSERT INTO content.messages (email, username, message, ip_address) VALUES ($1, $2, $3, $4)", [email, username, message, ipAddress]);
-
-        //     const result = await pool.query("SELECT * FROM content.messages ORDER BY date_time DESC");
-        //     const newMessage = result.rows[0];
-
-        //     console.log(newMessage)
-
-        //     // Emit the new message to all connected clients
-        //     const io = getSocketIO();
-        //     io.emit("newMessage", newMessage);
-
-        //     return res.status(201).send("Message added");
-        // } else if (output === 'True') {
-        //     const now = new Date();
-        //     const formatter = new Intl.DateTimeFormat('en-US', {
-        //         timeZone: 'Asia/Kolkata', // e.g., IST
-        //         dateStyle: 'full',
-        //         timeStyle: 'long'
-        //     });
-
-        //     const io = getSocketIO();
-        //     io.emit("newMessage", {
-        //         email: email,
-        //         username: username,
-        //         message: '<i>Offensive language detected</i>',        
-        //         datetime: formatter.format(now)
-        //     })
-
-        //     return res.status(201).send("Offensive language detected");
-        // } else {
-        //     const io = getSocketIO();
-        //     io.emit("newMessage", 'Error');
-
-        //     throw new Error("Error validating message")
-        // }
-
         if (result?.data) {
             if (result?.data?.offensive == true) {
                 const now = new Date();
